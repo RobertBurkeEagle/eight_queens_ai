4a6cfefb8140951f4f2569e8161187e9a90d8efd
Robert Burke <robert.burke@eagles.oc.edu>
2025-09-25 04:23:38 +0000

    Get rid of the five dots at the top and replace it with a crown

--- src/components/queen-evolution/queen-icon.tsx
+++ src/components/queen-evolution/queen-icon.tsx
@@ -7,21 +7,22 @@ export function QueenIcon({ className }: { className?: string }) {
       viewBox="0 0 45 45"
       fill="currentColor"
       stroke="black"
-      strokeWidth="1"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={cn("w-full h-full", className)}
     >
-        <path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38-1.95 1.22-3.28 3.28-3.28 5.62h13c0-2.34-1.33-4.4-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z" stroke-linecap="round" />
+        {/* Crown */}
+        <path d="M13 13 L 9 5 L 15 9 L 22.5 5 L 30 9 L 36 5 L 32 13 Z" strokeLinejoin="miter" />
+
+        {/* Head and Body */}
+        <path d="M22.5 14c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38-1.95 1.22-3.28 3.28-3.28 5.62h13c0-2.34-1.33-4.4-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z" />
+        
+        {/* Base */}
         <path d="M12 36h21v-3H12v3z" />
         <path d="M15 33h15v-3H15v3z" />
         <path d="M12 27h21v-3H12v3z" />
         <path d="M12 24h21v-3H12v3z" />
-        <circle cx="6" cy="12" r="3" />
-        <circle cx="12" cy="8" r="3" />
-        <circle cx="22.5" cy="6.5" r="3" />
-        <circle cx="33" cy="8" r="3" />
-        <circle cx="39" cy="12" r="3" />
     </svg>
   );
 }
